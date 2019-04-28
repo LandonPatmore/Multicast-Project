509b2908b82567d501056391d5faf5a61e73b1d9
Landon Patmore <landon.patmore@gmail.com>
2019-04-28 18:36:20 -0400

    Added basic game structure

---
+++ backend/src/main/java/game/Player.java
@@ -0,0 +1,46 @@
+package game;
+
+import utils.Utils;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Player {
+    private final String name;
+    private final int[] color;
+    private final List<Coordinates> snake;
+
+    private boolean isAlive;
+
+    public Player(String name) {
+        this.name = name;
+        this.color = Utils.generateColor();
+        this.snake = new LinkedList<>();
+        this.isAlive = true;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int[] getColor() {
+        return color;
+    }
+
+    public List<Coordinates> getSnake() {
+        return snake;
+    }
+
+    public void addToSnake(int x, int y) {
+        final Coordinates part = new Coordinates(x, y);
+        snake.add(part);
+    }
+
+    public boolean isAlive() {
+        return isAlive;
+    }
+
+    public void setAlive(boolean alive) {
+        isAlive = alive;
+    }
+}
