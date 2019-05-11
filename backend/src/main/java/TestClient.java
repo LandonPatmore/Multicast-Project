566be82833a4732ede0872d14e6d18a61d3c834c
Landon Patmore <landon.patmore@gmail.com>
2019-05-11 14:57:49 -0400

    changed test client to work with new byte codes

--- backend/src/main/java/TestClient.java
+++ backend/src/main/java/TestClient.java
@@ -16,7 +16,7 @@ public class TestClient {
                 try {
                     Thread.sleep(2000);
                     byte[] hBuf = new byte[1];
-                    hBuf[0] = 5;
+                    hBuf[0] = 3;
 
                     socket.send(new DatagramPacket(hBuf, hBuf.length, server, 4445));
                 } catch (InterruptedException | IOException e) {
@@ -29,19 +29,12 @@ public class TestClient {
 
         byte[] buf = new byte[test.length() + 1];
 
-        buf[0] = 2;
+        buf[0] = 1;
 
         for (int i = 0; i < test.length(); i++) {
             buf[i + 1] = test.getBytes()[i];
         }
 
         socket.send(new DatagramPacket(buf, buf.length, server, 4445));
-
-        while (true) {
-            byte[] errBuf = new byte[512];
-            final DatagramPacket packet = new DatagramPacket(errBuf, errBuf.length);
-            socket.receive(packet);
-            System.out.println(Arrays.toString(packet.getData()));
-        }
     }
 }
